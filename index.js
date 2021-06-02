34ac35ce5124b2239e4892e1536fcdf095e561f5
mutaliev06 <maga_mutaliev06@mail.ru>
2021-06-02 10:36:55 +0300

    min server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://Mutaliev:Intocode@cluster0.chyad.mongodb.net/pollsBackEnd', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(port, () => {
+            console.log('Сервер запущен...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
